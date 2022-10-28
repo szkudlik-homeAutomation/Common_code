252a52a0d8130a6d800e0784d2c81815f168dc53
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2022-10-28 12:05:38 +0200

    Init commit - add tSensorHub

---
+++ sensors/tSensorHub.cpp
@@ -0,0 +1,14 @@
+/*
+ * tSensorHub.cpp
+ *
+ *  Created on: Oct 28, 2022
+ *      Author: mszkudli
+ */
+
+#include "tSensorHub.h"
+
+tSensorHub::tSensorHub() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ sensors/tSensorHub.h
@@ -0,0 +1,16 @@
+/*
+ * tSensorHub.h
+ *
+ *  Created on: Oct 28, 2022
+ *      Author: mszkudli
+ */
+
+#ifndef SRC_COMMON_CODE_SENSORS_TSENSORHUB_H_
+#define SRC_COMMON_CODE_SENSORS_TSENSORHUB_H_
+
+class tSensorHub {
+public:
+	tSensorHub();
+};
+
+#endif /* SRC_COMMON_CODE_SENSORS_TSENSORHUB_H_ */
