8f88ffdb1a066a24eb7f21556c384d45925ab009
Marcin Szkudlinski <szkudlik@gmail.com>
2022-11-01 15:48:04 +0100

    Sensor hub implementation (WIP)

--- sensors/tSensorHub.cpp
+++ sensors/tSensorHub.cpp
@@ -6,10 +6,139 @@
  */
 
 #include "tSensorHub.h"
+#include "tSensor.h"
 
-tSensorHub::tSensorHub() {
-	// TODO Auto-generated constructor stub
+tSensorDesc * tSensorDesc::pFirst = NULL;
 
+
+tSensorDesc *tSensorDesc::getByID(uint8_t SensorID)
+{
+   tSensorDesc *pSensorDesc = pFirst;
+   while (pSensorDesc != NULL)
+   {
+      if (pSensorDesc->SensorID == SensorID)
+      {
+         return pSensorDesc;
+      }
+      pSensorDesc=pSensorDesc->pNext;
+   }
+
+   return pSensorDesc;
+}
+
+tSensorDesc *tSensorDesc::getByName(const char * pSensorName)
+{
+   tSensorDesc *pSensorDesc = pFirst;
+   while (pSensorDesc != NULL)
+   {
+      if (strcmp(pSensorDesc->pName, pSensorName) == 0)
+      {
+         return pSensorDesc;
+      }
+      pSensorDesc=pSensorDesc->pNext;
+   }
+
+   return pSensorDesc;
+}
+
+uint8_t tSensorHub::getSensorID(const char * pSensorName)
+{
+   tSensorDesc *pSensorDesc = tSensorDesc::getByName(pSensorName);
+   if (NULL == pSensorDesc)
+   {
+      return SENSOR_NOT_FOUND;
+   }
+
+   return pSensorDesc->SensorID;
+}
+
+uint8_t tSensorHub::getCachedSensorData(uint8_t SensorID,  uint8_t *dataBlobSize, void **pDataBlob)
+{
+   tSensorDesc *pSensorDesc = tSensorDesc::getByID(SensorID);
+   if (NULL == pSensorDesc)
+   {
+      return SENSOR_NOT_FOUND;
+   }
+
+   *dataBlobSize = pSensorDesc->dataBlobSize;
+   *pDataBlob = pSensorDesc->pDataCache;
+}
+
+void tSensorHub::CreateSensorRequest(uint8_t TargetNode, uint8_t SensorType, uint8_t SensorID, char * pSensorName, void* pConfigBlob, uint8_t MeasurementPeriod)
+{
+   //TODO: async sensor creation on external nodes
+
+   uint8_t Status;
+   (void) TargetNode; // not yet implemented
+
+   // create a sensor on a local node
+
+   Status = tSensor::Create(SensorType, SensorID);
+   if (Status != CREATE_SENSOR_STATUS_OK)
+   {
+      CreateSensorResponse(SensorID, Status);
+      return;
+   }
+
+   tSensor *pSensor = tSensor::getSensor(SensorID);
+   if (pSensor == NULL)
+   {
+      CreateSensorResponse(SensorID, CREATE_SENSOR_STATUS_OTHER_ERROR);
+      return;
+   }
+
+   Status = pSensor->SetSpecificConfig(pConfigBlob);
+   if (Status != CREATE_SENSOR_STATUS_OK)
+   {
+      CreateSensorResponse(SensorID, Status);
+      return;
+   }
+
+   pSensor->SetMeasurementPeriod(MeasurementPeriod);
+
+   // add sensor to repository
+   tSensorDesc *pSensorDesc = new tSensorDesc;
+   pSensorDesc->SensorID = SensorID;
+   pSensorDesc->pDataCache = NULL;
+   pSensorDesc->dataBlobSize = 0;
+   pSensorDesc->pName = pSensorName;
+   pSensorDesc->sensorType = SensorType;
+
+   // send response
+   CreateSensorResponse(SensorID, CREATE_SENSOR_STATUS_OK);
+}
+
+void tSensorHub::getSensorInfoRequest(uint8_t SensorID)
+{
+   //TODO: async communication with external nodes
+   tSensor *pSensor = tSensor::getSensor(SensorID);
+   tSensorDesc *pSensorDesc = tSensorDesc::getByID(SensorID);
+   if ((NULL == pSensor) || (NULL == pSensorDesc))
+   {
+      getSensorInfoResponse(SENSOR_NOT_FOUND, 0, 0, 0, 0, 0, NULL);
+      return;
+   }
+
+   getSensorInfoResponse(
+         CREATE_SENSOR_STATUS_OK,
+         pSensorDesc->SensorID,
+         0, // deviceID TBD
+         pSensorDesc->sensorType,
+         pSensor->isRunning(),
+         pSensor->GetMeasurementPeriod(),
+         pSensorDesc->pName);
+}
+
+uint8_t tSensorHub::subscribeToEvents(uint8_t SensorID, tSensorHubEvent *pSensorEvent)
+{
+   tSensorDesc *pSensorDesc = tSensorDesc::getByID(SensorID);
+   if (NULL == pSensorDesc)
+   {
+      return SENSOR_NOT_FOUND;
+   }
+
+   pSensorEvent->Connect(&pSensorDesc->pFirstEventHander);
+   return CREATE_SENSOR_STATUS_OK;
 }
 
 uint8_t tSensorHub::getCachedSensorDataJson(uint8_t SensorID, Stream *pStream)
--- sensors/tSensorHub.h
+++ sensors/tSensorHub.h
@@ -22,12 +22,34 @@ public:
    } tEventType;
 
 
-   tSensorHubEvent() : mpNext(NULL) {}
-   virtual ~tSensorHubEvent() {}
-
+   tSensorHubEvent() {}
    virtual void onEvent(uint8_t SensorID, tEventType EventType, uint8_t dataBlobSize, void *pDataBlob) = 0;
 private:
-   tSensorHubEvent *mpNext; friend class tSensorHub;
+   void Connect(tSensorHubEvent **pFirst) { pNext = *pFirst; *pFirst = this; }
+   tSensorHubEvent *pNext; friend class tSensorHub;
+};
+
+
+class tSensorDesc
+{
+public:
+   tSensorDesc() { pNext = pFirst; pFirst = this; }
+
+   uint8_t SensorID;
+   uint8_t sensorType;
+   uint8_t dataBlobSize;
+   void *pDataCache;
+   char * pName;
+   tSensorHubEvent *pFirstEventHander;
+
+   tSensorDesc *getFirst() { return pFirst; }
+   tSensorDesc *getNext() { return pNext; }
+
+   static tSensorDesc *getByID(uint8_t SensorID);
+   static tSensorDesc *getByName(const char * pSensorName);
+private:
+   tSensorDesc *pNext;
+   static tSensorDesc *pFirst;
 };
 
 
@@ -40,39 +62,52 @@ private:
  */
 class tSensorHub {
 public:
-	tSensorHub();
+
+	tSensorHub() {};
 
 	/*
 	 * Send a create sensor message / create a local sensor
 	 * virtual CreateSensorResponse will be called when operation is done
 	 *
+	 * @param TargetNode (TBD) a remote target ID where the sensor is to be created
+	 * @SensorType type of the sensor
+	 * @SensorID unqique ID of the sensor
+	 * @pSensorName poiner to sensor name in RAM (not in progmem), the name must be static, won't be copied to repo
+	 * @pConfigBlob pointer to sensor specific config structure
+	 * @MeasurementPeriod period of measurement (100ms unit)
 	 */
-	void CreateSensorRequest(uint8_t TargetNode, uint8_t SensorType, uint8_t sensorID, char * pSensorName);
+	void CreateSensorRequest(uint8_t TargetNode, uint8_t SensorType, uint8_t SensorID, char * pSensorName, void* pConfigBlob, uint8_t MeasurementPeriod);
+
 	/*
-	 * @retval 0 OK
-	 * @retval SENSOR_STATUS_DUPLICATE_ID
-	 * @retval SENSOR_STATUS_DUPLICATE_NAME
-	 * @retval SENSOR_STATUS_UNKNOWN_SENSOR
-	 * @retval SENSOR_STATUS_TIMEOUT
+	 * Method called when a response to CreateSensorRequest arrives
+	 * when sensor is on the same node, it may be called before CreateSensorRequest returns
+	 *
+	 * Status 0 OK
+	 *  SENSOR_STATUS_DUPLICATE_ID
+	 *  SENSOR_STATUS_DUPLICATE_NAME
+	 *  SENSOR_STATUS_UNKNOWN_SENSOR
+	 *  SENSOR_STATUS_TIMEOUT
+	 *  CREATE_SENSOR_STATUS_CONFIG_SET_ERROR
 	 */
-	virtual uint8_t CreateSensorResponse(uint8_t sensorID) {};
+	virtual void CreateSensorResponse(uint8_t SensorID, uint8_t Status) {};
 
 	/*
 	 * Get an ID of a sensor by name
 	 * Immediate response, local operation
 	 *
-	 * @retval SENSOR_STATUS_UNKNOWN_SENSOR
+	 * @retval SENSOR_NOT_FOUND
 	 * or sensor ID
 	 */
-	uint8_t getSensorID(char * pSensorName) const;
+	uint8_t getSensorID(const char * pSensorName);
 
 	/*
-	 * Get an ID of a sensor info
-	 * @retval SENSOR_STATUS_UNKNOWN_SENSOR
-	 * @retval SENSOR_STATUS_TIMEOUT
+	 * Get an ID of a sensor info (remote operation)
+	 * Status: SENSOR_NOT_FOUND
+	 *         SENSOR_STATUS_TIMEOUT
 	 */
 	void getSensorInfoRequest(uint8_t SensorID);
-	virtual uint8_t getSensorInfoResponse(
+	virtual void getSensorInfoResponse(
+	      uint8_t Status,
 			uint8_t SensorID,
 			uint8_t DeviceId,
 			uint8_t SensorType,
@@ -82,12 +117,12 @@ public:
 
 	/*
 	 * Subscribe to all events from the sensor
+	 * There's no unsubscribe - not needed in fact
 	 *
 	 * @retval SENSOR_STATUS_UNKNOWN_SENSOR
 	 * @retval SENSOR_STATUS_TIMEOUT
 	 */
-	uint8_t subscribeToEvents(uint8_t SensorId, tSensorHubEvent *pSensorEvent);
-	uint8_t unSubscribeToEvents(uint8_t SensorId);
+	uint8_t subscribeToEvents(uint8_t SensorID, tSensorHubEvent *pSensorEvent);
 
 	/**
 	 * Asyn get current sensor data
@@ -96,13 +131,13 @@ public:
 	 * @retval SENSOR_STATUS_TIMEOUT
 	 */
 	void getCurrentSensorDataRequest(uint8_t SensorID);
-	void getCurrentSensorDataResponse(uint8_t SensorID, uint8_t dataBlobSize, void *pDataBlob);
+	virtual void getCurrentSensorDataResponse(uint8_t SensorID, uint8_t dataBlobSize, void *pDataBlob) {}
 
 	/**
 	 * get cached sensor data
 	 * get data from a sensor stored locally
 	 *
-	 * @retval SENSOR_STATUS_UNKNOWN_SENSOR
+	 * @retval SENSOR_NOT_FOUND
 	 */
 	uint8_t getCachedSensorData(uint8_t SensorID,  uint8_t *dataBlobSize, void **pDataBlob);
 
@@ -113,7 +148,8 @@ public:
 	 * @retval SENSOR_STATUS_UNKNOWN_SENSOR
 	 */
 	uint8_t getCachedSensorDataJson(uint8_t SensorID, Stream *pStream);
-
 };
+
+extern tSensorHub SensorHub;
 
 #endif /* SRC_COMMON_CODE_SENSORS_TSENSORHUB_H_ */
