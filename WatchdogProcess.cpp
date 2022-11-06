3f6704a9c30657a34a9f8ee31d3e17561ccaaab2
Marcin Szkudlinski <szkudlik@gmail.com>
2022-11-06 13:17:34 +0100

    Extend watchdog to guard multiple points with diffrent timeouts

---
+++ WatchdogProcess.cpp
@@ -0,0 +1,40 @@
+#include "WatchdogProcess.h"
+#include <Watchdog.h>
+
+
+tWatchdogItem *tWatchdogItem::pFirst = NULL;
+
+bool tWatchdogItem::Tick() {
+   if (0 == mNumOfSeconds) return true;   // watchdog item disabled
+   if (mCurrentTimeoutCnt > 0)
+   {
+      mCurrentTimeoutCnt--;
+   }
+   else
+   {
+      return false;  // timeout
+   }
+   return true;
+}
+
+
+void tWatchdogProcess::service()
+{
+   tWatchdogItem *pItem = tWatchdogItem::pFirst;
+   bool doResetWatchdog = true;
+
+   while (NULL != pItem)
+   {
+      doResetWatchdog &= pItem->Tick();
+      pItem = pItem->pNext;
+   }
+
+   if (doResetWatchdog)
+   {
+      watchdog.reset();
+   }
+   else
+   {
+      DEBUG_PRINTLN_3("Watchdog goes off!");
+   }
+}
--- WatchdogProcess.h
+++ WatchdogProcess.h
@@ -1,7 +1,38 @@
+#pragma once
+
 #include "../../global.h"
 
 #include "../lib/external/ArduinoProcessScheduler/src/ProcessScheduler.h"
 #include <Watchdog.h>
+
+/**
+ * a base class for watchdog item that need to be triggered periodically
+ *
+ * if any of the items is not triggered at least once in required time the system must be restarted
+ */
+class tWatchdogItem
+{
+public:
+   tWatchdogItem(uint16_t NumOfSeconds)
+      : mNumOfSeconds(NumOfSeconds)
+   {
+      pNext = pFirst;
+      pFirst = this;
+      Reset();
+   }
+
+   void Reset() {mCurrentTimeoutCnt = mNumOfSeconds;}
+
+private:
+   bool Tick(); friend class tWatchdogProcess;
+
+   uint16_t mNumOfSeconds;
+   uint16_t mCurrentTimeoutCnt;
+
+   static tWatchdogItem *pFirst;
+   tWatchdogItem *pNext;
+};
+
 
 class tWatchdogProcess : public  Process
 {
@@ -9,12 +40,10 @@ class tWatchdogProcess : public  Process
   tWatchdogProcess(Scheduler &manager) : Process(manager, WATCHDOG_PRIORITY, SERVICE_SECONDLY, RUNTIME_FOREVER) {}
   virtual void setup()
   {
-    watchdog.enable(Watchdog::TIMEOUT_4S);
+    watchdog.enable(Watchdog::TIMEOUT_2S);
   }
-  virtual void service()
-  {
-    watchdog.reset();
-  }
+  virtual void service();
+
   private:
   Watchdog watchdog;
 };
