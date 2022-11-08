c989379bb865a149620e4d9eecebd1092b7ea596
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2022-11-08 15:45:44 +0100

    Add output control Jscript

---
+++ Network/servlets.h
@@ -0,0 +1,23 @@
+#ifndef SERVLETS
+#define SERVLETS
+
+#include "../../../global.h"
+#include "../../http_binaries.h"
+
+#include "httpServer.h"
+
+
+#ifdef OutputControl_js_raw_len
+
+class tOutputControlJavaScript : public tHttpServlet
+{
+public:
+  tOutputControlJavaScript() : tHttpServlet() {}
+  virtual ~tOutputControlJavaScript() {}
+
+  virtual bool ProcessAndResponse();
+};
+
+#endif
+
+#endif
