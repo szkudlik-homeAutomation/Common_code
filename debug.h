b75bc229b6c7ecc1da955e65bd79bd2039854f9b
Marcin Szkudlinski <szkudlik@gmail.com>
2022-11-11 17:57:53 +0100

    Better debug macros

--- debug.h
+++ debug.h
@@ -30,21 +30,32 @@
 
 #ifdef DEBUG_1
 #define DEBUG_PRINTLN_1(x) RespHandler.println(F(x));
+#define DEBUG_PRINT_1(x) RespHandler.print(F(x));
+#define DEBUG_1(x) RespHandler.x
 #else
 #define DEBUG_PRINTLN_1(x)
+#define DEBUG_PRINT_1(x)
+#define DEBUG_1(x)
 #endif
 
 #ifdef DEBUG_2
 #define DEBUG_PRINTLN_2(x) RespHandler.println(F(x));
+#define DEBUG_PRINT_2(x) RespHandler.print(F(x));
+#define DEBUG_2(x) RespHandler.x
 #else
 #define DEBUG_PRINTLN_2(x)
+#define DEBUG_PRINT_2(x)
+#define DEBUG_2(x)
 #endif
 
 #ifdef DEBUG_3
 #define DEBUG_PRINTLN_3(x) RespHandler.println(F(x));
-//#define DEBUG_PRINTLN_3(x) Serial.println(F(x));
+#define DEBUG_PRINT_3(x) RespHandler.print(F(x));
+#define DEBUG_3(x) RespHandler.x
 #else
 #define DEBUG_PRINTLN_3(x)
+#define DEBUG_PRINT_3(x)
+#define DEBUG_3(x)
 #endif
 
 int getFreeRam();
