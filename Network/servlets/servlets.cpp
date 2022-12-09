71002417072a893623868e588f8e65df026146ea
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2022-12-09 11:37:14 +0100

    Move Output control servlets to servlets dir

--- Network/servlets/servlets.cpp
+++ Network/servlets/servlets.cpp
@@ -2,11 +2,11 @@
 
 
 #if CONFIG_HTTP_SERVLET_OUTCONTROL_JS
-#ifdef OutputControl_js_raw_len
+
 bool tOutputControlJavaScript::ProcessAndResponse()
 {
 	pOwner->SendFlashString(OutputControl_js_raw,OutputControl_js_raw_len);
 	return false;
 }
-#endif
+
 #endif //CONFIG_HTTP_SERVLET_OUTCONTROL_JS
---
+++ Network/servlets/tOutputControlServlets.h
@@ -0,0 +1,43 @@
+/*
+ * tOutputSetServlet.h
+ *
+ *  Created on: Dec 9, 2022
+ *      Author: mszkudli
+ */
+
+#pragma once
+
+#include "../../../../global.h"
+#include "servlets.h"
+#include "../../OutputProcess.h"
+
+/**
+ * output servlet - controlling or checking state of an output
+ */
+class tOutputSetServlet :  public tHttpServlet
+{
+public:
+  tOutputSetServlet(tOutputProcess *aOutputProcess) :
+	  pOutputProcess(aOutputProcess),
+	  tHttpServlet() {}
+  virtual ~tOutputSetServlet() {}
+
+  virtual bool ProcessAndResponse();
+private:
+  tOutputProcess *pOutputProcess;
+};
+
+class tOutputStateServlet : public tHttpServlet
+{
+  public:
+   tOutputStateServlet(tOutputProcess *aOutputProcess)  :
+  	  pOutputProcess(aOutputProcess),
+  	  tHttpServlet() {}
+
+   virtual ~tOutputStateServlet() {}
+
+   virtual bool ProcessAndResponse();
+  private:
+    tOutputProcess *pOutputProcess;
+};
+
