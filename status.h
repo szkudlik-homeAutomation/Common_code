673af0adaf76c3200eb56995e85861ea4aae7018
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2023-01-04 13:07:11 +0100

    restore status.s and helpers.h

---
+++ status.h
@@ -0,0 +1,20 @@
+/*
+ * status.h
+ *
+ *  Created on: 27 lis 2022
+ *      Author: szkud
+ */
+
+#pragma once
+
+#define STATUS_SUCCESS 0
+#define STATUS_GENERAL_FAILURE 1
+#define STATUS_DUPLICATE_ID 2
+#define STATUS_UNKNOWN_SENSOR_TYPE 3
+#define STATUS_UNKNOWN_SENSOR_ID 4
+#define STATUS_CONFIG_SET_ERROR 5
+#define STATUS_SENSOR_CREATE_ERROR 6
+#define STATUS_JSON_ENCODE_ERROR 6
+#define STATUS_SENSOR_ERROR_REPORTED 7
+#define STATUS_NO_DATA_RECIEVED 8
+#define STATUS_INCORRECT_DATA_SIZE 9
