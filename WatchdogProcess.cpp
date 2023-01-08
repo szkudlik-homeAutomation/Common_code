1a881c1124a2a311e3eae714a0174371b0d53d8a
Marcin Szkudlinski <szkudlik@gmail.com>
2023-01-08 18:55:06 +0100

    MERGE - NOTE - watchdog has changes!!!!! restore watchdog

---
+++ WatchdogProcess.cpp
@@ -0,0 +1,44 @@
+#include "WatchdogProcess.h"
+#include <Watchdog.h>
+#include "../lib/Ethernet/src/localEthernet.h"
+
+tWatchdogItem *tWatchdogItem::pFirst = NULL;
+
+bool tWatchdogItem::Tick() {
+   if (0 == mNumOfSeconds) return true;   // watchdog item disabled
+   if (mCurrentTimeoutCnt > 0)
+   {
+      mCurrentTimeoutCnt--;
+   }
+   else
+   {
+      return false;  // timeout
+   }
+   return true;
+}
+
+
+void tWatchdogProcess::service()
+{
+   tWatchdogItem *pItem = tWatchdogItem::pFirst;
+   bool doResetWatchdog = true;
+
+   while (NULL != pItem)
+   {
+      bool isOK = pItem->Tick();
+      doResetWatchdog &= isOK;
+      if (! isOK)
+      {
+         // an item has not been reset in required time
+         // try reset...
+         pItem->doRecovery();
+      }
+      pItem = pItem->pNext;
+   }
+
+   if (doResetWatchdog)
+   {
+      // set in HW that everything is fine
+      watchdog.reset();
+   }
+}
---
+++ WatchdogProcess.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "../../global.h"
+
+#include "../lib/ArduinoProcessScheduler/src/ProcessScheduler.h"
+#include <Watchdog.h>
+
+/**
+ * a base class for watchdog item that need to be triggered periodically
+ *
+ * if any of the items is not triggered at least once in required time the system must be restarted
+ */
+class tWatchdogItem
+{
+public:
+   tWatchdogItem(uint16_t NumOfSeconds)
+      : mNumOfSeconds(NumOfSeconds)
+   {
+      pNext = pFirst;
+      pFirst = this;
+      Reset();
+   }
+
+   void Reset() {mCurrentTimeoutCnt = mNumOfSeconds;}
+
+   virtual void doRecovery() {}
+
+private:
+   bool Tick(); friend class tWatchdogProcess;
+
+   uint16_t mNumOfSeconds;
+   uint16_t mCurrentTimeoutCnt;
+
+   static tWatchdogItem *pFirst;
+   tWatchdogItem *pNext;
+};
+
+
+class tWatchdogProcess : public  Process
+{
+  public:
+  tWatchdogProcess(Scheduler &manager) : Process(manager, WATCHDOG_PRIORITY, SERVICE_SECONDLY, RUNTIME_FOREVER) {}
+  virtual void setup()
+  {
+    watchdog.enable(Watchdog::TIMEOUT_8S);
+  }
+  virtual void service();
+
+  private:
+  Watchdog watchdog;
+};
