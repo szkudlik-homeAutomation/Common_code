cf7be464683be20347b0ac6e8f75cf8ff0fad682
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2023-01-13 09:27:16 +0100

    Add Logger class

---
+++ Logger.h
@@ -0,0 +1,65 @@
+/*
+ * Logger.h
+ *
+ *  Created on: Jan 12, 2023
+ *      Author: mszkudli
+ */
+
+#pragma once
+
+#include "../../global.h"
+#if CONFIG_LOGGER
+
+#include "helpers.h"
+#include "Print.h"
+
+class tLogger : public Print {
+public:
+	tLogger() : mLogsForced(false) {}
+	virtual ~tLogger() {}
+
+    virtual size_t write(uint8_t str);
+
+    void EnableLogsForce() { mLogsForced = true; }
+    void DisableLogsForce() { mLogsForced = false; }
+
+private:
+	  bool mLogsForced;
+};
+
+extern tLogger Logger;
+
+class tLogTransport
+{
+	friend class tLogger;
+public:
+	tLogTransport() :mLogEnbabled(false) { pNext = pFirst ; pFirst = this; }
+	virtual ~tLogTransport();
+
+	virtual void Log(uint8_t str)  = 0;
+
+    void EnableLogs() { mLogEnbabled = true; }
+	void DisableLogs() { mLogEnbabled = false; }
+
+private:
+	bool mLogEnbabled;
+	static tLogTransport* pFirst;
+	tLogTransport *pNext;
+};
+
+#ifdef DEBUG_SERIAL
+class tSerialLogTransport : public tLogTransport
+{
+public:
+	tSerialLogTransport() : tLogTransport() {EnableLogs();}
+	virtual ~tSerialLogTransport() {};
+
+	virtual void Log(uint8_t str) { DEBUG_SERIAL.write(str); }
+};
+
+extern tSerialLogTransport SerialLogTransport;
+
+#endif
+
+
+#endif // CONFIG_LOGGER
