1d7f681b4d320016b7ad626e01cd2440a1d63321
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2023-01-13 09:27:16 +0100

    MERGE: Add Logger class

--- debug.h
+++ debug.h
@@ -11,36 +11,40 @@
 
 #include "../../global.h"
 
-#if CONFIG_CENTRAL_NODE
+#if !CONFIG_LOGGER
 
-//#define DEBUG_1_ENABLE
-//#define DEBUG_2_ENABLE
-#define DEBUG_3_ENABLE
+#undef DEBUG_1_ENABLE
+#undef DEBUG_2_ENABLE
+#undef DEBUG_3_ENABLE
 
-#define DEBUG_SERIAL Serial
-#define DEBUG_SERIAL_EVENT serialEvent
+#define LOG_PRINTLN(x)
+#define LOG_PRINT(x)
+#define LOG(x)
 
-#include "../ResponseHandler.h"
+#else // !CONFIG_LOGGER
 
-#endif CONFIG_CENTRAL_NODE
+#include "Logger.h"
 
+#define LOG_PRINTLN(x) { Logger.EnableLogsForce(); Logger.println(F(x)); Logger.DisableLogsForce(); }
+#define LOG_PRINT(x)   { Logger.EnableLogsForce(); Logger.print(F(x)); Logger.DisableLogsForce(); }
+#define LOG(x) 		   { Logger.EnableLogsForce(); Logger.x; ; Logger.DisableLogsForce(); }
 
-
+#endif // !CONFIG_LOGGER
 
 #ifdef DEBUG_1_ENABLE
-#define DEBUG_PRINTLN_1(x) RespHandler.println(F(x));
-#define DEBUG_PRINT_1(x) RespHandler.print(F(x));
-#define DEBUG_1(x) RespHandler.x
+#define DEBUG_PRINTLN_1(x) Logger.println(F(x));
+#define DEBUG_PRINT_1(x) Logger.print(F(x));
+#define DEBUG_1(x) Logger.x
 #else
 #define DEBUG_PRINTLN_1(x)
 #define DEBUG_PRINT_1(x)
 #define DEBUG_1(x)
 #endif
 
-#ifdef DEBUG_2_ENABLE
-#define DEBUG_PRINTLN_2(x) RespHandler.println(F(x));
-#define DEBUG_PRINT_2(x) RespHandler.print(F(x));
-#define DEBUG_2(x) RespHandler.x
+#ifdef DEBUG_1_ENABLE
+#define DEBUG_PRINTLN_2(x) Logger.println(F(x));
+#define DEBUG_PRINT_2(x) Logger.print(F(x));
+#define DEBUG_2(x) Logger.x
 #else
 #define DEBUG_PRINTLN_2(x)
 #define DEBUG_PRINT_2(x)
@@ -48,14 +52,13 @@
 #endif
 
 #ifdef DEBUG_3_ENABLE
-#define DEBUG_PRINTLN_3(x) RespHandler.println(F(x));
-#define DEBUG_PRINT_3(x) RespHandler.print(F(x));
-#define DEBUG_3(x) RespHandler.x
+#define DEBUG_PRINTLN_3(x) Logger.println(F(x));
+#define DEBUG_PRINT_3(x) Logger.print(F(x));
+#define DEBUG_3(x) Logger.x
 #else
 #define DEBUG_PRINTLN_3(x)
 #define DEBUG_PRINT_3(x)
 #define DEBUG_3(x)
 #endif
-
 int getFreeRam();
 
