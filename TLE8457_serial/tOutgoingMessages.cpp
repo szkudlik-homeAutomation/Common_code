95e2a34deac2bd44a3b742bdaba170611ab9f4ba
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2023-10-10 14:51:52 +0200

    Add empty tOutgoingMessages

---
+++ TLE8457_serial/tOutgoingMessages.cpp
@@ -0,0 +1,14 @@
+/*
+ * tOutgoingMessages.cpp
+ *
+ *  Created on: Oct 10, 2023
+ *      Author: mszkudli
+ */
+
+#include "tOutgoingMessages.h"
+
+tOutgoingMessages::tOutgoingMessages() {
+    // TODO Auto-generated constructor stub
+
+}
+
---
+++ TLE8457_serial/tOutgoingMessages.h
@@ -0,0 +1,19 @@
+/*
+ * tOutgoingMessages.h
+ *
+ *  Created on: Oct 10, 2023
+ *      Author: mszkudli
+ */
+
+#pragma once
+
+#include "../../../global.h"
+
+#if CONFIG_TLE8457_COMM_LIB
+
+class tOutgoingMessages {
+public:
+    tOutgoingMessages();
+};
+
+#endif /* CONFIG_TLE8457_COMM_LIB */
