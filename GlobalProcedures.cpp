a5b7fba7ecc0414195c81d20888c8037798760b5
szkudlik <szkudlik@gmail.com>
2024-07-08 07:26:49 +0200

    watchdog - working

--- GlobalProcedures.cpp
+++ GlobalProcedures.cpp
@@ -9,13 +9,22 @@
 #include "../../global.h"
 
 #include "../lib/ArduinoProcessScheduler/src/ProcessScheduler.h"
-
-void loop() {
-  sched.run();
-}
+#include "../Common_code/WatchdogProcess.h"
 
 
 void setup() {
+
+#if CONFIG_WATCHDOG
+  WatchdogProcess.add(true);
+#endif
+
+#if CONFIG_LOGGER
+  CONFIG_LOGGER_SERIAL.begin(115200);
+  while (!CONFIG_LOGGER_SERIAL);
+  CONFIG_LOGGER_SERIAL.print(F("START, v"));
+  CONFIG_LOGGER_SERIAL.println(FW_VERSION);
+#endif
+
 #if CONFIG_TLE8457_COMM_LIB
 	// Initialize ID of the node
 
@@ -23,4 +32,9 @@ void setup() {
 	// CommSerialSetID(EEPROM.read(EEPROM_DEVICE_ID_OFFSET));
 
 #endif // CONFIG_TLE8457_COMM_LIB
+}
+
+
+void loop() {
+  sched.run();
 }
