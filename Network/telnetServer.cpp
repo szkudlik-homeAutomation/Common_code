b74ee71ccaac04a75df5bcd893b911a6ad8d65b4
szkudlik <szkudlik@gmail.com>
2024-08-16 17:03:04 +0200

    Add a default telnet commands table

--- Network/telnetServer.cpp
+++ Network/telnetServer.cpp
@@ -375,4 +375,38 @@ bool send_restoreSensorsFromEeprom(Commander &Cmdr)
 #endif // CONFIG_TELNET_COMMANDS_SENSORS
 #endif // CONFIG_TLE8457_COMM_LIB
 
+const commandList_t TelnetCommands[] = {
+  {"enableLogs",      TelnetEnableLogs,             "enable logs on telnet console"},
+  {"disableLogs",     TelnetDisableLogs,            "enable logs on telnet console"},
+  {"GetVersion",      send_GetVersion,              "show version"},
+  {"Reset",           send_Reset,                   "reset the system"},
+#if CONFIG_NODE_SCAN_TASK
+  {"ScanActiveNodes", trigger_ScanNodes,            "Scan the bus for nodes from 1 to 32"},
+#endif //CONFIG_NODE_SCAN_TASK
+#if CONFIG_TLE8457_COMM_LIB
+#if CONFIG_OUTPUT_PROCESS
+  {"StateOverview",   send_stateOverviewHandler,    "StateOverview dev_id"},
+  {"OutputState",     send_OutputStateHandler,      "OutputState dev_id out_id"},
+  {"SetOutput",       send_SetOutputHandler,        "SetOutput dev_id out_id state [timer] (not set=default, 0-forever)"},
+#endif // CONFIG_OUTPUT_PROCESS
+#if CONFIG_TELNET_COMMANDS_SENSORS
+  {"GetSensorById",   send_GetSensorByIdReqestHandler,"GetSensorById sensor_id [dst_dev = broadcast]"},
+  {"GetSensorMeasurement",  send_GetSensorMeasurementReqest, "GetSensorMeasurement sensor_id [dst_dev = broadcast]"},
+  {"CreateSensor", send_CreateSensorRequest, "CreateSensor dev_id sensor_type sensor_id"},
+  {"StartSensor", send_StartSensorRequest, "StartSensor sensor_id [sensor_ev_mask = EV_TYPE_MEASUREMENT_COMPLETED] [dev_id = broadcast]"},
+  {"StopSensor", send_StopSensorRequest, "StopSensor sensor_id [dev_id = broadcast]"},
+  {"ConfigureSensor", send_ConfigureSensorRequest, "ConfigureSensor sensor_id period [dev_id = broadcast]"},
+  {"SaveSensorsToEeprom", send_saveSensorsToEeprom, "SaveSensorsToEeprom dst_dev"},
+  {"RestoreSensorsFromEeprom", send_restoreSensorsFromEeprom, "RestoreSensorsFromEeprom dst_dev"},
+#endif // CONFIG_TELNET_COMMANDS_SENSORS
+#endif //CONFIG_TLE8457_COMM_LIB
+
+#ifdef TELNET_LOCAL_COMMANDS
+	TELNET_LOCAL_COMMANDS
+#endif //TELNET_LOCAL_COMMANDS
+
+};
+
+tTelnetServer TelnetServer(TelnetCommands,sizeof(TelnetCommands));
+
 #endif // CONFIG_TELNET_SERVER
