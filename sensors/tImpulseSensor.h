3efd12cdefefdc341985662a20f2ffd66d6d09bb
Szkudlik <szkudlik@gmail.com>
2025-06-17 22:06:57 +0200

    tImpulseSensor json

--- sensors/tImpulseSensor.h
+++ sensors/tImpulseSensor.h
@@ -8,31 +8,36 @@
 #pragma once
 
 #include "../../../global.h"
-#if CONFIG_IMPULSE_SENSOR
-
 #include "tSensor.h"
 #include "tSensorCache.h"
 #include "tSensorLogger.h"
 
-#if CONFIG_SENSORS_JSON_OUTPUT
-
+#if CONFIG_IMPULSE_SENSOR_JSON_OUTPUT
 uint8_t ImpulseSensorJsonFormat_api_1(Stream *pStream, tSensorCache *cache);
+#endif //CONFIG_IMPULSE_SENSOR_JSON_OUTPUT
 
-#endif //CONFIG_SENSORS_JSON_OUTPUT
-
-class tImpulseSensor final : public tSensor {
+#if CONFIG_IMPULSE_SENSOR || CONFIG_IMPULSE_SENSOR_JSON_OUTPUT
+class tImpulseSensorTypes
+{
 public:
-   typedef struct
-   {
-      uint16_t Count; // numer of impulses in last period
-      uint16_t Sum;   // sum of all impulses
-   } tResult_api_v1;
+	   typedef struct
+	   {
+	      uint16_t Count; // numer of impulses in last period
+	      uint16_t Sum;   // sum of all impulses
+	   } tResult_api_v1;
 
-   typedef struct
-   {
-      uint8_t Pin;
-   } tConfig_api_v1;
+	   typedef struct
+	   {
+	      uint8_t Pin;
+	   } tConfig_api_v1;
+};
+#endif
 
+
+#if CONFIG_IMPULSE_SENSOR
+
+class tImpulseSensor final : public tSensor, public tImpulseSensorTypes {
+public:
    static const uint8_t API_VERSION = 1;
    typedef tConfig_api_v1 tConfig;
    typedef tResult_api_v1 tResult;
