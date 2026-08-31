b94e00a5002d90b7986c13aa7f827924c3273500
szkudlik <szkudlik@gmail.com>
2026-08-31 22:45:19 +0200

    after AI fix

--- tMessageReciever.cpp
+++ tMessageReciever.cpp
@@ -43,14 +43,13 @@ void tMessageReciever::Dispatch(uint8_t type, uint16_t data, void *pData)
 	tMessageReciever *pCurrent = pFirst;
 	while (pCurrent != NULL)
 	{
+		tMessageReciever *pNext = pCurrent->pNext;
+
 		if (pCurrent->mMessageMask & ((uint32_t)1 << type))
 		{
-			tMessageReciever *pNext = pCurrent->pNext;
 			pCurrent->onMessage(type,data,pData);
-			pCurrent = pNext;
-			continue;
 		}
 
-		pCurrent=pCurrent->pNext;
+	  pCurrent = pNext;
 	}
 }
